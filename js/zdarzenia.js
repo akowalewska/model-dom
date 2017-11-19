78961af09823487dfae9f1233eb3df75c5026b3e
Alexandra Kowalewska <kowalewska.alexandra@gmail.com>
2017-11-19 15:24:32 +0100

    another events

--- js/zdarzenia.js
+++ js/zdarzenia.js
@@ -1,29 +1,88 @@
-document.addEventListener('DOMContentLoaded', function(){
-//    tutaj nazwy funkcji, odpalane przy danym zdarzeniu
+document.addEventListener('DOMContentLoaded', function () {
+    //    tutaj nazwy funkcji, odpalane przy danym zdarzeniu
     changeColor();
     hoverLinks();
+    wcisnijPodnies();
+    wypiszZInputa();
+    wypiszDoKonsoli();
 })
 
+window.addEventListener('resize', function () {
+    zmienCos();
+})
+
+window.addEventListener('scroll', function () {
+
+})
+
+
+
 function changeColor() {
-//tworze zmienna, trzyma tablice z elementami o klasie
+    //tworze zmienna, trzyma tablice z elementami o klasie
     var linki = document.getElementsByClassName('link');
-    
-    for(var i=0; i < linki.length; i++) {
+
+    for (var i = 0; i < linki.length; i++) {
         linki[i].style.color = 'red';
-        
+
     }
 }
 
-function hoverLinks(){
+function hoverLinks() {
     var linki = document.getElementsByClassName('link');
-    for(var i=0; i < linki.length; i++) {
+    for (var i = 0; i < linki.length; i++) {
         linki[i].style.transition = 'all 0.4s';
-        
-        linki[i].addEventListener('mouseover', function(){
+
+        linki[i].addEventListener('mouseover', function () {
             this.style.color = 'blue';
         })
-        linki[i].addEventListener('mouseout', function(){
+        linki[i].addEventListener('mouseout', function () {
             this.style.color = 'red';
+        })
+
+        linki[i].addEventListener('click', function () {
+            this.style.fontSize = '25px';
         })
     }
 }
+
+function wcisnijPodnies() {
+    var inputs = document.querySelectorAll('#form input');
+    //    console.log(inputs);
+    for (var i = 0; i < inputs.length; i++) {
+        inputs[i].addEventListener('keydown', function () {
+            this.style.background = 'green';
+        })
+
+        inputs[i].addEventListener('keyup', function () {
+            this.style.background = 'initial';
+        })
+    }
+}
+
+function wypiszZInputa() {
+    var inputy = document.querySelectorAll('#form input');
+    
+    for (var i=0; i < inputy.length; i++) {
+        inputy[i].addEventListener('change', function() {
+            console.log(this.value);
+        })
+    }
+}
+
+function zmienCos() {
+    if (window.outerWidth < 768) {
+        document.body.style.backgroundColor = 'black';
+    } else {
+        document.body.style.backgroundColor = 'initial';
+    }
+}
+
+function wypiszDoKonsoli() {
+    console.log('123');
+}
+
+var form = document.getElementById('form');
+
+//przypisanie funkcji do eventu po samej nazwie (nie uzywamy nawiasow)
+form.addEventListener('click', wypiszDoKonsoli);
+form.removeEventListener('click', wypiszDoKonsoli);
